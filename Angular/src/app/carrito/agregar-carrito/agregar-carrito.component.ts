b49b3a97b4522755f442fb34e8b184460440c8dc
Lilian Morales Quevedo <lsmorales.23@est.ucab.edu.ve>
2025-05-27 21:31:33 -0400

    más componentes agregar-carrito (ts, css, spec.ts)

---
+++ Angular/src/app/carrito/agregar-carrito/agregar-carrito.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-agregar-carrito',
+  imports: [],
+  templateUrl: './agregar-carrito.component.html',
+  styleUrl: './agregar-carrito.component.css'
+})
+export class AgregarCarritoComponent {
+
+}
