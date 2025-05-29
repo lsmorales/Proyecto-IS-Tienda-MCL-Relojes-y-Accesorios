10dc29ea3b217f89d1782db028edef0f378ca72a
marco <mdargonis.23@est.ucab.edu.ve>
2025-05-29 11:49:00 -0400

    Final -1

--- Angular/src/app/app.routes.ts
+++ Angular/src/app/app.routes.ts
@@ -2,25 +2,31 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './perfil/login/login.component';
 import { RegisterComponent } from './perfil/register/register.component';
 import { ControlPaginaMainComponent } from './gestion-pagina-main/control-pagina-main/control-pagina-main.component';
-import { CarritoComponent } from './gestion-carrito/carrito/carrito.component'; // Importa el componente del carrito
+import { CarritoComponent } from './gestion-carrito/carrito/carrito.component';
 import { authGuard } from './guards/auth.guard';
 import { adminGuard } from './guards/admin.guard';
 import { RegistrarPagoComponent } from './pagos/registrar-pago/registrar-pago.component';
+import { ConfirmarPagoComponent } from './pagos/confirmar-pago/confirmar-pago.component'; // Asegúrate de importar el componente
 
 export const routes: Routes = [
   { path: '', component: ControlPaginaMainComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path:'registrar-pago', component: RegistrarPagoComponent },
+  { path: 'registrar-pago', component: RegistrarPagoComponent },
   { 
     path: 'admin', 
     component: ControlPaginaMainComponent,
     canActivate: [authGuard, adminGuard] 
-  }, 
+  },
+  { 
+    path: 'confirmar-pagos', // Nueva ruta para confirmar pagos
+    component: ConfirmarPagoComponent,
+    canActivate: [authGuard, adminGuard] // Solo accesible para admins
+  },
   { 
     path: 'carrito', 
     component: CarritoComponent,
-    canActivate: [authGuard] // Solo usuarios autenticados
+    canActivate: [authGuard]
   },
   { path: '**', redirectTo: '' }
 ];
